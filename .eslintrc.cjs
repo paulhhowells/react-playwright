5466c91799602fb8eb6851b898e4d928c85d6095
Paul H Howells <p.h.howells@gmail.com>
2023-10-21 23:03:15 +0100

    chore: add vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,18 +26,19 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'warn',
       },
     },
-    // {
-    //   // E2E Playwright
-    //   files: ['e2e/*.ts'],
-    //   rules: {
-    //     '@typescript-eslint/no-explicit-any': 'off', // why doesn't this work?
-    //   },
-    // },
+    {
+      // E2E Playwright
+      files: [ 'e2e/*.ts' ],
+      'extends': [ 'plugin:playwright/recommended' ],
+      // rules: {
+      //   '@typescript-eslint/no-explicit-any': 'off', // why doesn't this work?
+      // },
+    },
 		{
       // Vitest & React Testing Library
 			// 3) Now we enable eslint-plugin-testing-library rules or preset only for matching testing files!
-			files: ['**/__tests__/**/*.[jt]s?(x)', 'src/**/?(*.)+(spec|test).[jt]s?(x)'],
-      plugins: ['testing-library', 'vitest'],
+      files: [ '**/__tests__/**/*.[jt]s?(x)', 'src/**/?(*.)+(spec|test).[jt]s?(x)' ],
+      plugins: [ 'testing-library', 'vitest' ],
 			extends: [
         'plugin:testing-library/react',
         'plugin:vitest/recommended',
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [ react() ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+
+    include: [ 'src/**/*.test.?(c|m)[jt]s?(x)' ],
+    exclude: [ ...configDefaults.exclude, '**/e2e/**', '**/playwright/**' ],
+  },
+});
